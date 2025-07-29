2320b6c7506ac5a0e095e886fa67554affd7e78b
m_Abdullah_Ali <mabdullahali8642@gmail.com>
2025-07-30 01:46:57 +0500

    front end completed almost

--- ai_fitness_trainer_client/src/main.jsx
+++ ai_fitness_trainer_client/src/main.jsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
 import App from './App.jsx';
+import './index.css';
 
 const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// Convex client banao
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={clerkKey}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
